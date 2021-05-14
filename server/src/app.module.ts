7f9c2ecf37ae9151d823a35a6d339595f34fa07c
Viet Do <viet.do@saleshood.com>
2021-05-14 23:52:45 +0700

    Integrate mongodb with mongoose

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,15 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app/app.controller';
 import { UsersController } from './users/users.controller';
+
 import { AppService } from './app/services/app.service';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 
+const mongoURL = "mongodb+srv://quocviet:123@cluster0.tlb6l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(mongoURL)],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
